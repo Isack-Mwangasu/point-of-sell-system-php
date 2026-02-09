46d9687338580b66550091f2e302aacbcd38fd19
isak <mwangasuisack@gmail.com>
2026-02-09 13:33:14 +0300

    created navbar

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.7-dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap-5.3.7-dist/js/bootstrap.bundle.min.js"></script>
+    <title>Point of Sale System</title>
+</head>
+<body>
+    <?php include 'links.php'; ?>
+
+    <div class="container">
+    Home
+    </div>
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm fixed-top">
+	<div class="container">
+		<a class="navbar-brand fw-semibold" href="index.php">POS</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="mainNavbar">
+			<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+				<li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+				<li class="nav-item"><a class="nav-link" href="sell.php">Sell</a></li>
+				<li class="nav-item"><a class="nav-link" href="links.php">Links</a></li>
+				<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
